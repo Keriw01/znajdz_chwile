9affd59cdab7ab46e464bb96cfe82bb57ba79ec2
Keriw01 <sebas342@o2.pl>
2023-05-02 18:25:23 +0200

    Settings Page

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:znajdz_chwile/pages/stats.dart';
 
 import '../users/authentication/login_screen.dart';
 import '../users/userPreferences/user_preferences.dart';
+import '../colors/colors.dart';
 
 final GoogleSignIn _googleSignOut = GoogleSignIn();
 
@@ -13,15 +15,74 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: color2,
       body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              RememberUserPrefs.removeUserInfo().then((value) {
-                _googleSignOut.signOut();
-                Get.offAll(const LoginScreen());
-              });
-            },
-            child: const Text("Wyloguj")),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                RememberUserPrefs.removeUserInfo().then((value) {
+                  _googleSignOut.signOut();
+                  Get.offAll(const LoginScreen());
+                });
+              },
+              style: ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsets>(
+                      const EdgeInsets.fromLTRB(75, 10, 75, 10)),
+                  backgroundColor: MaterialStateProperty.all(color8),
+                  shape: MaterialStateProperty.all(const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))))),
+              child: const Text(
+                "Wyloguj",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Segoe UI',
+                    fontWeight: FontWeight.w700),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                          title: const Text("Znajdz Chwilę"),
+                          content: const Text(
+                              "Aplikacja stworzona przez grupę projektową. © 2023"),
+                          actions: <Widget>[
+                            TextButton(
+                              child: const Text(
+                                'Cofnij',
+                                style: TextStyle(color: color8),
+                              ),
+                              onPressed: () {
+                                Get.back();
+                              },
+                            ),
+                          ],
+                        ));
+              },
+              style: ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsets>(
+                      const EdgeInsets.fromLTRB(65, 10, 65, 10)),
+                  backgroundColor: MaterialStateProperty.all(color8),
+                  shape: MaterialStateProperty.all(const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))))),
+              child: const Text(
+                "Informacje",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Segoe UI',
+                    fontWeight: FontWeight.w700),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
